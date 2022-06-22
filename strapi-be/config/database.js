bd0f94ff8dbe4333cc951318b85046aefdf7631f
David Ho <1073113@gmail.com>
2022-06-22 10:02:33 +0800

    Update database.js

--- strapi-be/config/database.js
+++ strapi-be/config/database.js
@@ -1,13 +1,14 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: env("DATABASE_CLIENT", "postgres"),
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'database-strapi.chmpleaodutp.ap-southeast-1.rds.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'postgresDBAWS'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'postgres'),
       ssl: env.bool('DATABASE_SSL', false),
     },
+    useNullAsDefault: true,
   },
 });
